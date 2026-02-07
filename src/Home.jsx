4dfe5f890ef20e491a51ac878023ff2038a8d457
Shafiq-Ahamed <shafiqahamed263@gmail.com>
2026-02-07 17:06:42 +0530

    First Commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import { useState,useRef } from "react";
-import "./home.css"
+import "./Home.css"
 import { useNavigate } from "react-router-dom";
 import {GoTasklist} from "react-icons/go";
 const Home = () => {
